e3991310a154418fe607b8286bfc52e4c6498b12
XiaoYiS <yihaiyang1203@sina.com>
2015-12-10 11:57:50 +0800

    创建
    创建目录

---
+++ ForU/ForU/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ForU
+//
+//  Created by yihaiyang on 15/12/10.
+//  Copyright © 2015年 yihaiyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ForU/ForU/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ForU
+//
+//  Created by yihaiyang on 15/12/10.
+//  Copyright © 2015年 yihaiyang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
